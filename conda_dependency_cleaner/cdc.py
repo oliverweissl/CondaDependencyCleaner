64b553f6661aa0836a901b3d704740f7cd2b10b6
oliverweissl <oliver.weissl@outlook.com>
2025-06-04 14:43:05 +0200

    Added new functionality

--- conda_dependency_cleaner/cdc.py
+++ conda_dependency_cleaner/cdc.py
@@ -1,7 +1,9 @@
 import argparse
+import sys
 
 from ._clean_environment_from_file import clean_environment_from_file
-from._convert_pip_to_conda import convert_pip_to_conda
+from ._convert_pip_to_conda import convert_pip_to_conda
+from .utility import configure_global_logger
 
 
 def main() -> None:
@@ -9,12 +11,14 @@ def main() -> None:
     parser = argparse.ArgumentParser(
         prog="CondaDependencyCleaner",
         description="A helper tool to clean your conda environments and convert pip dependencies to conda.",
-        add_help=False,
     )
-    subparsers = parser.add_subparsers(dest="command")
+    subparsers = parser.add_subparsers(dest="command", help="Available subcommands", required=True)
 
     """Parse arguments for cleaning."""
-    clean_parser = subparsers.add_parser("clean", help="Clean conda environments from transitive dependencies.")
+    clean_parser = subparsers.add_parser(
+        "clean", help="Clean conda environments from transitive dependencies."
+    )
+    clean_parser.set_defaults(func=clean_environment_from_file)
     clean_parser.add_argument("filename", type=str, help="The conda environment file to clean.")
     clean_parser.add_argument(
         "-nf",
@@ -33,17 +37,15 @@ def main() -> None:
         help="Allows to exclude builds of the dependencies.",
         action="store_true",
     )
-    clean_parser.add_argument(
-        "-h",
-        "--help",
-        action="help",
-        default=argparse.SUPPRESS,
-        help="Show this help message and exit.",
-    )
 
     """Parse arguments for converting."""
-    convert_parser = subparsers.add_parser("convert", help="Convert pip dependencies to conda dependencies.")
-    convert_parser.add_argument("filename", type=str, help="The conda environment file to use for conversion.")
+    convert_parser = subparsers.add_parser(
+        "convert", help="Convert pip dependencies to conda dependencies."
+    )
+    convert_parser.set_defaults(func=convert_pip_to_conda)
+    convert_parser.add_argument(
+        "filename", type=str, help="The conda environment file to use for conversion."
+    )
     convert_parser.add_argument(
         "-nf",
         "--new-filename",
@@ -51,31 +53,21 @@ def main() -> None:
         help="The new conda environment filename.",
         required=False,
     )
-    convert_parser.add_argument("-c", "--channels", nargs="+", help="The channels to use for conversion, seperated by spaces.")
     convert_parser.add_argument(
-        "-h",
-        "--help",
-        action="help",
-        default=argparse.SUPPRESS,
-        help="Show this help message and exit.",
+        "-c",
+        "--channels",
+        nargs="+",
+        help="The channels to use for conversion, seperated by spaces. (default: defaults conda-forge)",
+        default=["defaults", "conda-forge"],
     )
 
-    args = parser.parse_args()
-    if args.command == "clean":
-        clean_environment_from_file(
-            args.filename,
-            args.new_filename,
-            exclude_versions=args.exclude_versions,
-            exclude_builds=args.exclude_builds,
-        )
-    elif args.command == "convert":
-        convert_pip_to_conda(
-            args.filename,
-            args.new_filename,
-            channels=args.channels,
-        )
-    else:
+    if len(sys.argv) == 1:
         parser.print_help()
+        sys.exit(1)
+
+    configure_global_logger()
+    args = parser.parse_args()
+    args.func(**vars(args))
 
 
 if __name__ == "__main__":
--- conda_dependency_cleaner/utility/__init__.py
+++ conda_dependency_cleaner/utility/__init__.py
@@ -2,6 +2,7 @@
 
 from ._dependency import Dependency
 from ._get_dependeny_graph import get_dependency_graph
+from ._logging import configure_global_logger
 from ._to_yaml_patch import to_yaml_patch
 
-__all__ = ["to_yaml_patch", "get_dependency_graph", "Dependency"]
+__all__ = ["to_yaml_patch", "get_dependency_graph", "Dependency", "configure_global_logger"]
